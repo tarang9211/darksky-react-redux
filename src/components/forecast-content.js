eb0dee893a4dc571a9e63071869f6e2291a92600
Tarang Hirani <tarang9211@gmail.com>
2017-02-25 20:31:35 +0530

    fixed minor typo

--- src/components/forecast-content.js
+++ src/components/forecast-content.js
@@ -4,7 +4,7 @@ const ForecastContent = ({ maxTemp, minTemp }) => (
   <div className='content'>
     <div className='content-temp'>
       <p className='content-max-temp' title='Maximum Temperature'>{maxTemp.toFixed(0)}&deg;</p>
-      <p className='content-min-temp' title='Minimum Temperature'>{minTemp.toFixed(0)}&deg;</p>
+      <p className='content-min-temp' title='Minumum Temperature'>{minTemp.toFixed(0)}&deg;</p>
     </div>
   </div>
 )

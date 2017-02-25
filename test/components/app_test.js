262409ed8b89285c043f262cef9efb377a8a51b0
Tarang Hirani <tarang9211@gmail.com>
2017-02-26 01:02:43 +0530

    fixing test cases

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,5 +1,7 @@
 import { renderComponent , expect } from '../test_helper';
+import faker from 'faker';
 import App from '../../src/components/app';
+import ForecastHeader from '../../src/components/forecast-header';
 
 describe('App' , () => {
   let component;
@@ -8,7 +10,36 @@ describe('App' , () => {
     component = renderComponent(App);
   });
 
-  it('renders something', () => {
+  it('should render the app component', () => {
     expect(component).to.exist;
   });
+});
+
+describe('ForecastHeader', () => {
+  let component;
+
+  const speed = faker.random.number({min: 1, max: 10, precision:0.0001});
+
+  const props = {
+    weather: {
+      windSpeed: 5.43,
+      time: 14780000,
+    }
+  }
+
+  beforeEach(() => {
+    component = renderComponent(ForecastHeader, props);
+  });
+
+  it('should render the ForecastHeader component', () => {
+    expect(component).to.exist;
+  });
+
+  it('should display the windSpeed prop', () => {
+    console.log(window.$('.header'));
+    console.log(component);
+  });
+
+
+
 });
